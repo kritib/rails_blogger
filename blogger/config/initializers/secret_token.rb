264c33c3ab8b78b259da00a79f74c611a2ad4770
JordanHamel <jo.mravca@gmail.com>
2013-01-30 13:23:26 -0800

    starting rails blogger, empty setup so far

---
+++ blogger/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blogger::Application.config.secret_token = '4d1203d8a5aacbb9c5aba10cb49bec09c1b9d070ea29bb357ecaf4ce2a3aef384a450f05ed61aa9da23395854ccac21e6f8850db03078605f8ba04497f760b0e'
